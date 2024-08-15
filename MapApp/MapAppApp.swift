99eb36190411e833fdf8795210bbfa7d5d30d1ee
Tharusha <tharushalakshan3@gmail.com>
2024-08-15 07:06:21 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Tharusha Lakshan on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
