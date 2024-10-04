1a3eef97c6dbfbd2aba607d2df45bc01af21a772
Tharusha <tharushalakshan3@gmail.com>
2024-10-04 15:31:39 +0530

    [Feature] add locationPreviewView, LocationsView

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -21,10 +21,12 @@ class LocationsViewModel: ObservableObject {
             updateMapRegion(location: mapLocation)
         }
     }
-    
+    //current region on map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
-    
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    //show list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
@@ -39,5 +41,44 @@ class LocationsViewModel: ObservableObject {
                 center: location.coordinates,
                 span: mapSpan)
         }
+    }
+    
+    
+    func toggleLocationsLiat(){
+        withAnimation(.easeInOut){
+            showLocationsList = !showLocationsList
+        }
+    }
+    
+    func showNextLocation(location:Location){
+        withAnimation(.easeInOut){
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
+    
+    func nextButtonPressed(){
+        //Get the current index
+//        let currentIndex = locations.firstIndex { location in
+//        return location == mapLocation}
+        
+        guard let currentIndex = locations.firstIndex(where: {$0 == mapLocation}) else{
+            print("could not find current index in locations array! Should never happen.")
+            return
+        }
+        
+        //check of the nextIndex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            //Next index is Not valid
+            // Restart from 0
+            guard let firstLocation = locations.first else {return}
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        //Next index is valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
     }
 }
--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -18,11 +18,72 @@ struct LocationsView: View {
     var body: some View {
         ZStack{
             Map(coordinateRegion: $vm.mapRegion)
+            
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0){
+                
+               header
+                .padding()
+                
+                Spacer()
+                
+                ZStack{
+                    ForEach(vm.locations){ location in
+                        if vm.mapLocation == location {
+                            LocationPreviewView(location: location)
+                                .shadow(color: Color.black.opacity(0.3),radius: 20)
+                                .padding()
+                                .transition(.asymmetric(
+                                    insertion: .move(edge: .trailing),
+                                    removal: .move(edge: .leading)))
+                        }
+                       
+                    }
+                }
+            }
             
         }
     }
 }
+
+
+extension LocationsView{
+    
+    private var header: some View{
+        VStack {
+            Button(action: vm.toggleLocationsLiat){
+                Text(vm.mapLocation.name + "," + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation)
+                    .overlay(alignment: .leading){
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
+                    }
+                
+            }
+            if vm.showLocationsList{
+                LocationsListView()
+            }
+            
+            
+        }
+        .background(.thinMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x:0, y:15)
+    }
+    
+}
+
+
+
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
