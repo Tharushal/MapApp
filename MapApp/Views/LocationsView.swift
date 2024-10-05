22e0a88bdce49b678ff7455de3696cae1dd1e485
Tharusha <tharushalakshan3@gmail.com>
2024-10-05 12:41:07 +0530

    [Feature] add LocationDetailView

--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -12,6 +12,7 @@ import MapKit
 struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
 //    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
 //        center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
 //    
@@ -23,10 +24,15 @@ struct LocationsView: View {
             VStack(spacing: 0){
                header
                 .padding()
+                .frame(maxWidth: maxWidthForIpad)
                 Spacer()
                locationsPreviewStack
             }
             
+        }
+        
+        .sheet(item: $vm.sheetLocation, onDismiss: nil) { location in
+            LocationDetailView(location: location)
         }
     }
 }
@@ -89,6 +95,8 @@ extension LocationsView{
                     LocationPreviewView(location: location)
                         .shadow(color: Color.black.opacity(0.3),radius: 20)
                         .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
                         .transition(.asymmetric(
                             insertion: .move(edge: .trailing),
                             removal: .move(edge: .leading)))
